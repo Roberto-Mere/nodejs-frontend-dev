26da85ec076c1a8e64a79df93689556930494ab9
rmere <rmere@griddynamics.com>
2025-05-26 11:01:49 -0600

    Restore template form UI

--- index.js
+++ index.js
@@ -3,15 +3,23 @@ import cors from 'cors';
 import('dotenv').then((dotenv) => dotenv.config());
 import { initializeDatabase } from './src/db/db.js';
 import userRoutes from './src/routes/userRoutes.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
 const port = process.env.PORT || 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 await initializeDatabase();
 
 app.use(cors());
+app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
 app.use('/api/users', userRoutes);
 
 app.use((req, res, next) => {
